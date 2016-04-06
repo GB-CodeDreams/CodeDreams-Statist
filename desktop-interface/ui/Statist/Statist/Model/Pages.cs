cd633cbb2b38518fc7bded4eba9e1e5d3cad5abd
shtylev <shtylevAA@rambler.ru>
2016-04-06 10:21:02 +0300

    Добавление класса Pages

---
+++ desktop-interface/ui/Statist/Statist/Model/Pages.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Statist.Model
+{
+    class Pages
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public int SiteId { get; set; }
+        public DateTime FoundDateTime { get; set; }
+        public DateTime LastScanDate { get; set; }
+        public ICollection<PersonPageRanks> PersonPageRanks { get; set; } = new List<PersonPageRanks>();
+    }
+}
