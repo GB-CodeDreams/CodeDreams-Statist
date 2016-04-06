68c9ee8c7e20b0e3d95fafcf34c75d5aed5006bc
shtylev <shtylevAA@rambler.ru>
2016-04-06 10:04:54 +0300

    Добавлены два класса: Persons и PersonPageRanks

---
+++ desktop-interface/ui/Statist/Statist/Model/PersonPageRanks.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Statist.Model
+{
+    class PersonPageRanks
+    {
+        public int PersonId { get; set; }
+        public int PageId { get; set; }
+        public int Rank { get; set; }
+    }
+}
