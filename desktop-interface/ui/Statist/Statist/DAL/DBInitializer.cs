82c4ad202cfda0cfe326c970e15adaaf2d6eefd6
shtylev <shtylevAA@rambler.ru>
2016-04-06 18:25:48 +0300

    Добавлен метод FillPages

--- desktop-interface/ui/Statist/Statist/DAL/DBInitializer.cs
+++ desktop-interface/ui/Statist/Statist/DAL/DBInitializer.cs
@@ -26,5 +26,30 @@ namespace Statist.DAL
             sites.Add(siteRia);
             return sites;
         }
+        public static List<Pages> FillPages()
+        {
+            List<Pages> pages = new List<Pages>();
+
+            Pages pageLenta = new Pages()
+            {
+                Id = 1,
+                Url = "lenta.ru",
+                SiteId = 1,
+                FoundDateTime = new DateTime(2016, 04, 06, 18, 05, 30),
+                LastScanDate = new DateTime(2016, 04, 06, 18, 13, 30)
+            };
+            Pages pageRia = new Pages()
+            {
+                Id = 2,
+                Url = "ria.ru",
+                SiteId = 2,
+                FoundDateTime = new DateTime(2016, 04, 06, 18, 10, 30),
+                LastScanDate = new DateTime(2016, 04, 06, 18, 23, 30)
+            };
+
+            pages.Add(pageLenta);
+            pages.Add(pageRia);
+            return pages;
+        }
     }
 }
