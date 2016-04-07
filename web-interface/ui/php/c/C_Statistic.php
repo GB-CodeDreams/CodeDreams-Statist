6305f0d3769c6c1c7ff8ed7a4ec6195445b889e1
dimireme <a_s_d@mail.ru>
2016-04-07 22:45:36 +0600

    Create page general_statistics with temporal data.

--- web-interface/ui/php/c/C_Statistic.php
+++ web-interface/ui/php/c/C_Statistic.php
@@ -7,10 +7,20 @@ class C_Statistic extends C_Base {
 	}
 
 	public function action_general_statistics(){
-
-		$message = isset($_POST['site']) ? $_POST['site'] : "NULL";
+		$general_statistics = array();
+		$isSelectSite = false;
 		
-		$vars = array('site' => $message);
+		//temporal data
+		$site = "lenta";
+
+		if ($this->IsPost()) {
+			$isSelectSite = true;
+			$site = $_POST['site'];
+			//temporal data
+			$general_statistics = array("Pytin" => "146", "Medved" => "79");
+		} 	
+		
+		$vars = array('general_statistics' => $general_statistics, 'isSelectSite' => $isSelectSite, 'site' => $site);
 
 		$this->title .= '::Общая статистика';
 		$this->content = $this->Template('v/v_general_statistics.php', $vars);
--- web-interface/ui/php/v/v_general_statistics.php
+++ web-interface/ui/php/v/v_general_statistics.php
@@ -2,14 +2,41 @@
 Шаблон страницы общей статистики
 =======================
 
+$general_statistics - массив строк общей статистики
+$isSelectSite - хранит информацию о том, выбран ли сайт для отображения статистики
+$site - название сайта, выбранного для отображения статистики
 */?>
 
 <form method="post" action="index.php?c=statistic&act=general_statistics">
-	<select name="site">
+	<select name="site" id="site">
 		<option value="lenta">lenta.ru</option>
 		<option value="kp">kp.ru</option>
 	</select>
 	<input type="submit" value="Применить">
 </form>
 
-<?=$site?>
+<?/* Скрипт сохраняет выбранный сайт в поле 'Select' */?>
+<script type="text/javascript">
+  	document.getElementById('site').value = "<?php echo $site;?>";
+</script>
+
+
+
+<?php if ($isSelectSite): ?>
+	
+	<table>
+		<tr>
+			<th>Имя</th>
+			<th>Количество упоминаний</th>
+		</tr>
+
+		<?php foreach ($general_statistics as $name => $count): ?>
+			<tr>
+				<td><?=$name?></td>
+				<td><?=$count?></td>
+			</tr>
+		<?php endforeach ?>
+
+	</table>
+
+<?php endif ?>
