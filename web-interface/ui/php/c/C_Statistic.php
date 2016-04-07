2bbb00403fabf57cc22dc2f5d7e6fcdc5fd2137b
dimireme <a_s_d@mail.ru>
2016-04-08 01:24:22 +0600

    Add dates to page daily_stats

--- web-interface/ui/php/c/C_Statistic.php
+++ web-interface/ui/php/c/C_Statistic.php
@@ -2,6 +2,8 @@
 
 class C_Statistic extends C_Base {
 
+	const  ONE_WEEK = 7*24*60*60;
+
 	public function action_index() {
 		$this->title .= '::Главная страница';
 	}
@@ -37,14 +39,16 @@ class C_Statistic extends C_Base {
 		$daily_stats = array();
 		$total_count = 0;
 		$isError = false;
-
+		$start_date = date('Y-m-d', time() - self::ONE_WEEK);
+		$end_date = date('Y-m-d', time());
+		
 		//temporal data
 		$sites = array("lenta.ru", "kp.ru", "any_other_site.com");
 		$persons = array("Pytin", "Medvedev", "Noval'ny");
 
 		$selected_site = $sites[0];
 		$selected_person = $persons[0];
-
+		
 		if ($this->IsPost()) {
 			
 			if (isset($_POST['site'])) {
@@ -59,6 +63,14 @@ class C_Statistic extends C_Base {
 				$isError = true;
 			}
 
+			if ( strlen($_POST['start_date']) == 0 || strlen($_POST['end_date']) == 0) {
+				$isError = true;
+			}
+
+			$end_date = $_POST['end_date'];
+			$start_date = $_POST['start_date'];
+
+			
 			if(!$isError) {
 				//temporal data
 				$daily_stats = array("01.04.2016" => "146", "02.04.2016" => "79", "03.04.2016" => "15");
@@ -71,10 +83,12 @@ class C_Statistic extends C_Base {
 		$vars = array(
 			'daily_stats' => $daily_stats,
 			'isError' => $isError,
-			'selected_site' => $selected_site,
-			'selected_person' => $selected_person,
 			'sites' => $sites,
 			'persons' => $persons,
+			'selected_site' => $selected_site,
+			'selected_person' => $selected_person,
+			'start_date' => $start_date,
+			'end_date' => $end_date,
 			'total_count' => $total_count
 		);
 		$this->title .= '::Ежедневная статистика';
--- web-interface/ui/php/v/v_daily_stats.php
+++ web-interface/ui/php/v/v_daily_stats.php
@@ -6,6 +6,8 @@ $daily_stats - массив строк ежедневной статистики
 $isError - хранит информацию о том, есть ли ошибка в заполнении формы
 $selected_site - название сайта, выбранного для отображения статистики
 $selected_person - личность, выбранная для отображения статистики
+$start_date - дата, выбранная пользователем, или дата по-умолчанию
+$end_date - дата, выбранная пользователем, или дата по-умолчанию
 $sites - массив сайтов, доступных для просмотра статистики
 $persons - массив личностей, доступных для просмотра статистики
 $tatal_count - суммарное количество новых упоминаний
@@ -13,12 +15,14 @@ $tatal_count - суммарное количество новых упомина
 */?>
 
 <form method="post" action="index.php?c=statistic&act=daily_stats">
+
 	Сайт:
 	<select name="site" id="site">
 		<?php foreach ($sites as $site): ?>
 			<option value="<?=$site?>"><?=$site?></option>
 		<?php endforeach ?>
 	</select>
+	<br>
 
 	Личность:
 	<select name="person" id="person">
@@ -26,18 +30,23 @@ $tatal_count - суммарное количество новых упомина
 			<option value="<?=$person?>"><?=$person?></option>
 		<?php endforeach ?>
 	</select>
+	<br>
 
 	Период с:
-
+	<input type="date" name="start_date" id="start_date" max="2017-06-04" min="2000-01-01">
 	по: 
-
+	<input type="date" name="end_date" id="end_date" max="2017-06-04" min="2000-01-01">
+	
 	<input type="submit" value="Применить">
+
 </form>
 
-<?/* Скрипт сохраняет выбранный сайт, личность в форме */?>
+<?/* Скрипт сохраняет выбранный сайт, личность и даты в форме */?>
 <script type="text/javascript">
   	document.getElementById('site').value = "<?php echo $selected_site;?>";
   	document.getElementById('person').value = "<?php echo $selected_person;?>";
+  	document.getElementById('start_date').value = "<?php echo $start_date;?>";
+  	document.getElementById('end_date').value = "<?php echo $end_date;?>";
 </script>
 
 
