3de1f2f9cabae76f26f01532e9a773de6210ac79
Kirill <kerzo@mail.ru>
2016-04-07 15:03:12 +0000

    day_statistic

--- web-service/models/person_page_rank.rb
+++ web-service/models/person_page_rank.rb
@@ -3,9 +3,38 @@ class PersonPageRank < ActiveRecord::Base
   belongs_to :page
 
   scope :site_persons_rank, ->(site) {
-    person_rank = "SELECT sum(r.rank) as rank, per.name as person_name, s_p.name as site_name FROM person_page_ranks r JOIN persons per ON r.person_id = per.id"
-    site_pages = "SELECT s.name, p.id FROM sites AS s INNER JOIN pages AS p ON s.id = p.site_id WHERE (s.name = '#{site}')"
-    find_by_sql("#{person_rank} JOIN (#{site_pages}) as s_p ON s_p.id = r.page_id GROUP BY site_name, person_name")
+    person_rank = %{
+      SELECT sum(r.rank) as rank, per.name as person_name, s_p.name as site_name 
+      FROM   person_page_ranks r 
+      JOIN   persons per ON r.person_id = per.id
+    }.gsub(/\s+/, ' ').strip
+    
+    site_pages = %{
+      SELECT s.name, p.id FROM sites AS s 
+      INNER JOIN pages AS p ON s.id = p.site_id 
+      WHERE (s.name = '#{site}')
+    }.gsub(/\s+/, ' ').strip
+    
+    find_by_sql("#{person_rank} JOIN (#{site_pages}) as s_p ON s_p.id = r.page_id GROUP BY person_name")
+  }
+  
+  scope :day_statistic, ->(site, person, start_date, end_date) {
+    person_rank = %{
+      SELECT sum(r.rank) as rank, per.name as person_name, s_p.name as site_name, s_p.date 
+      FROM   person_page_ranks r 
+      JOIN   persons per ON r.person_id = per.id
+    }.gsub(/\s+/, ' ').strip
+    
+    site_pages = %{
+      SELECT      s.name, p.id, p.last_scan_date as date 
+      FROM        sites AS s 
+      INNER JOIN  pages AS p ON s.id = p.site_id 
+      WHERE       (s.name = '#{site}' 
+        AND p.last_scan_date BETWEEN STR_TO_DATE('#{start_date}', '%Y-%m-%d %H:%i:%s') 
+        AND STR_TO_DATE('#{end_date}', '%Y-%m-%d %H:%i:%s'))
+    }.gsub(/\s+/, ' ').strip
+    
+    find_by_sql("#{person_rank} JOIN (#{site_pages}) as s_p ON s_p.id = r.page_id WHERE (per.name = '#{person}') GROUP BY s_p.date ORDER BY s_p.date DESC")
   }
 
   def self.hash_result_without_id(query_result)
@@ -15,5 +44,6 @@ class PersonPageRank < ActiveRecord::Base
       hash
     end
   end
+  
 
 end
