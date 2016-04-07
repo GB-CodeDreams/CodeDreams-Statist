ef656754b3e78693beb00c5dc48251b5ffbd2cb3
dimireme <a_s_d@mail.ru>
2016-04-07 23:00:36 +0600

    Add array 'sites' for display to user.

--- web-interface/ui/php/c/C_Statistic.php
+++ web-interface/ui/php/c/C_Statistic.php
@@ -9,19 +9,26 @@ class C_Statistic extends C_Base {
 	public function action_general_statistics(){
 		$general_statistics = array();
 		$isSelectSite = false;
-		
+
 		//temporal data
-		$site = "lenta";
+		$sites = array("lenta.ru", "kp.ru", "any_other_site.com");
+
+		$selected_site = $sites[0];
 
 		if ($this->IsPost()) {
 			$isSelectSite = true;
-			$site = $_POST['site'];
+			$selected_site = $_POST['site'];
+			
 			//temporal data
 			$general_statistics = array("Pytin" => "146", "Medved" => "79");
 		} 	
-		
-		$vars = array('general_statistics' => $general_statistics, 'isSelectSite' => $isSelectSite, 'site' => $site);
 
+		$vars = array(
+			'general_statistics' => $general_statistics,
+			'isSelectSite' => $isSelectSite,
+			'selected_site' => $selected_site,
+			'sites' => $sites
+		);
 		$this->title .= '::Общая статистика';
 		$this->content = $this->Template('v/v_general_statistics.php', $vars);
 	}
--- web-interface/ui/php/v/v_general_statistics.php
+++ web-interface/ui/php/v/v_general_statistics.php
@@ -4,20 +4,23 @@
 
 $general_statistics - массив строк общей статистики
 $isSelectSite - хранит информацию о том, выбран ли сайт для отображения статистики
-$site - название сайта, выбранного для отображения статистики
+$selected_site - название сайта, выбранного для отображения статистики
+$sites - массив сайтов, доступных для просмотра статистики
+
 */?>
 
 <form method="post" action="index.php?c=statistic&act=general_statistics">
 	<select name="site" id="site">
-		<option value="lenta">lenta.ru</option>
-		<option value="kp">kp.ru</option>
+		<?php foreach ($sites as $site): ?>
+			<option value="<?=$site?>"><?=$site?></option>
+		<?php endforeach ?>
 	</select>
 	<input type="submit" value="Применить">
 </form>
 
 <?/* Скрипт сохраняет выбранный сайт в поле 'Select' */?>
 <script type="text/javascript">
-  	document.getElementById('site').value = "<?php echo $site;?>";
+  	document.getElementById('site').value = "<?php echo $selected_site;?>";
 </script>
 
 
