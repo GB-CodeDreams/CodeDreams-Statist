14bfe1c18cd897a6fc6056e523519d61b492835e
indablackk <indablackk@gmail.com>
2016-04-08 19:10:30 +0400

    Создал модели для всех таблиц с помощью SQLAlchemy

---
+++ crawler/crawler/models.py
@@ -0,0 +1,106 @@
+#!/usr/bin/python3
+
+from datetime import datetime
+from sqlalchemy import Column, ForeignKeyConstraint
+from sqlalchemy.dialects.mysql import INT, DATETIME, VARCHAR
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class Persons(Base):
+    """ Persons entity class """
+    __tablename__ = 'persons'
+
+    id = Column('id', INT(unsigned=True), nullable=False, primary_key=True)
+    name = Column(VARCHAR(2048), nullable=False)
+
+    __table_args__ = {
+        'mysql_engine': 'InnoDB',
+        'mysql_charset': 'utf8'
+    }
+
+    def __init__(self, name):
+        self.name = name
+
+
+class Keywords(Base):
+    __tablename__ = 'keywords'
+
+    id = Column('id', INT(unsigned=True), nullable=False, primary_key=True)
+    name = Column('name', VARCHAR(2048), nullable=False)
+    person_id = Column('person_id', INT(unsigned=True), nullable=False)
+
+    __table_args__ = (ForeignKeyConstraint([person_id], [Persons.id],
+                                           name='fk_keywords_persons',
+                                           ondelete='CASCADE'),
+                      {'mysql_engine': 'InnoDB',
+                       'mysql_charset': 'utf8'}
+                      )
+
+    def __init__(self, name, person_id):
+        self.name = name
+        self.person_id = person_id
+
+
+class Sites(Base):
+    """ Sites entity class """
+    __tablename__ = 'sites'
+
+    id = Column('id', INT(unsigned=True), nullable=False, primary_key=True)
+    name = Column('name', VARCHAR(256), nullable=False)
+
+    __table_args__ = {
+        'mysql_engine': 'InnoDB',
+        'mysql_charset': 'utf8'
+    }
+
+    def __init__(self, name):
+        self.name = name
+
+
+class Pages(Base):
+    """ Pages entity class """
+    __tablename__ = 'pages'
+
+    id = Column('id', INT(unsigned=True), nullable=False, primary_key=True)
+    url = Column('url', VARCHAR(2048), nullable=False)
+    site_id = Column('site_id', INT(unsigned=True), nullable=False)
+    found_date_time = Column(DATETIME, default=datetime.utcnow)
+    last_scan_date = Column(DATETIME)
+
+    __table_args__ = (ForeignKeyConstraint([site_id], [Sites.id],
+                                           name='fk_pages_sites',
+                                           ondelete='CASCADE'),
+                      {'mysql_engine': 'InnoDB',
+                       'mysql_charset': 'utf8'}
+                      )
+
+    def __init__(self, url, site_id):
+        self.url = url
+        self.site_id = site_id
+
+
+class Person_page_ranks(Base):
+    """ Person_page_ranks entity class """
+    __tablename__ = 'person_page_ranks'
+
+    person_id = Column('person_id', INT(unsigned=True), nullable=False)
+    page_id = Column('page_id', INT(unsigned=True), nullable=False)
+    rank = Column('rank', INT(unsigned=True), nullable=False)
+
+    __table_args__ = (ForeignKeyConstraint([person_id], [Persons.id],
+                                           name='fk_ranks_persons',
+                                           ondelete='CASCADE'),
+                      ForeignKeyConstraint([page_id], [Pages.id],
+                                           name='fk_ranks_pages',
+                                           ondelete='CASCADE'),
+                      {'mysql_engine': 'InnoDB',
+                       'mysql_charset': 'utf8'}
+                      )
+
+    def __init__(self, person_id, page_id, rank):
+        self.person_id = person_id
+        self.page_id = page_id
+        self.rank = rank
