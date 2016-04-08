4ecb680bd74479cc1be705471a1a7e40a656a8e6
Kirill <kerzo@mail.ru>
2016-04-08 09:07:26 +0000

    error if query date incorrect

--- web-service/models/page.rb
+++ web-service/models/page.rb
@@ -1,4 +1,8 @@
 class Page < ActiveRecord::Base
   has_many    :person_page_ranks, dependent: :destroy
   belongs_to  :site
+  
+  scope :last_scan, -> {
+    maximum("last_scan_date")
+  }
 end
--- web-service/models/person_page_rank.rb
+++ web-service/models/person_page_rank.rb
@@ -19,22 +19,30 @@ class PersonPageRank < ActiveRecord::Base
   }
 
   scope :day_statistic, ->(site, person, start_date, end_date) {
-    person_rank = %{
-      SELECT sum(r.rank) as rank, per.name as query_word, s_p.name as site, s_p.date
-      FROM   person_page_ranks r
-      JOIN   persons per ON r.person_id = per.id
-    }.gsub(/\s+/, ' ').strip
-
-    site_pages = %{
-      SELECT      s.name, p.id, p.last_scan_date as date
-      FROM        sites AS s
-      INNER JOIN  pages AS p ON s.id = p.site_id
-      WHERE       (s.name = '#{site}'
-        AND p.last_scan_date BETWEEN STR_TO_DATE('#{start_date}', '%Y-%m-%d %H:%i:%s')
-        AND STR_TO_DATE('#{end_date}', '%Y-%m-%d %H:%i:%s'))
-    }.gsub(/\s+/, ' ').strip
-
-    find_by_sql("#{person_rank} JOIN (#{site_pages}) as s_p ON s_p.id = r.page_id WHERE (per.name = '#{person}') GROUP BY s_p.date ORDER BY s_p.date DESC")
+    result = ''
+    clear_error
+    validate_date(start_date, end_date)
+    if valid_date? 
+      person_rank = %{
+        SELECT sum(r.rank) as rank, per.name as query_word, s_p.name as site, s_p.date
+        FROM   person_page_ranks r
+        JOIN   persons per ON r.person_id = per.id
+      }.gsub(/\s+/, ' ').strip
+    
+      site_pages = %{
+        SELECT      s.name, p.id, p.last_scan_date as date
+        FROM        sites AS s
+        INNER JOIN  pages AS p ON s.id = p.site_id
+        WHERE       (s.name = '#{site}'
+          AND p.last_scan_date BETWEEN STR_TO_DATE('#{start_date}', '%Y-%m-%d %H:%i:%s')
+          AND STR_TO_DATE('#{end_date}', '%Y-%m-%d %H:%i:%s'))
+      }.gsub(/\s+/, ' ').strip
+    
+      result = find_by_sql("#{person_rank} JOIN (#{site_pages}) as s_p ON s_p.id = r.page_id WHERE (per.name = '#{person}') GROUP BY s_p.date ORDER BY s_p.date DESC")
+    else
+      result = error
+    end
+    result
   }
 
   def self.hash_result_without_id(query_result)
@@ -44,6 +52,28 @@ class PersonPageRank < ActiveRecord::Base
       hash
     end
   end
-
-
+    
+  def self.validate_date(start_date, end_date)
+    begin
+      start_date = start_date.to_datetime
+      end_date = end_date.to_datetime
+      @error = [{error: "no statistic for that period" }] if end_date <= Page.last_scan
+      @error = [{error: "start_date more then end_date"}] if start_date > end_date
+    rescue ArgumentError => e
+      @error = [{error: e.message }]
+    end
+  end
+  
+  def self.valid_date?
+    @error.nil?
+  end
+  
+  def self.clear_error
+    @error = nil
+  end
+  
+  def self.error
+    @error
+  end
+  
 end
