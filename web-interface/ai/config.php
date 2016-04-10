ac89b64d661a77d43a119db3b25e0a28913b98c4
dimireme <a_s_d@mail.ru>
2016-04-11 01:35:34 +0600

    Прописаны настройки удалённой БД

--- web-interface/ai/config.php
+++ web-interface/ai/config.php
@@ -1,7 +1,7 @@
 <?php
 return $db = array(
-    'hostname' => 'localhost',
-    'username' => 'root',
-    'password' => '',
-    'dbname' => 'statist_db');
+    'hostname' => 'us-cdbr-iron-east-03.cleardb.net',
+    'username' => 'bd941bf360b590',
+    'password' => '4a3b98d8',
+    'dbname' => 'heroku_e87588648c44590');
 
