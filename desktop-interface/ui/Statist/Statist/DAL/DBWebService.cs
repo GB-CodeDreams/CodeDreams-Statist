0b7cd68d52ed6130f3f4cf7b77927d078cc3e0a4
shtylev <shtylevAA@rambler.ru>
2016-04-11 22:42:17 +0300

    Добавил класс DBWebService

---
+++ desktop-interface/ui/Statist/Statist/DAL/DBWebService.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using Statist.Model;
+using Statist.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Statist.DAL
+{
+    public static class DBWebService
+    {
+        public static List<Persons> FillPersons()
+        {
+            string response = "";
+            List<Persons> persons = new List<Persons>();
+            using (var webClient = new WebClient())
+            {
+                try
+                {
+                    webClient.Encoding = Encoding.UTF8;
+                    response = webClient.DownloadString(Resources.GetPersons);                    
+                    persons = JsonConvert.DeserializeObject<List<Persons>>(response);
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }                
+            }
+            return persons.OrderBy(n => n.Name).ToList();
+        }
+        public static List<Sites> FillSites()
+        {
+            string response = "";
+            List<Sites> sites = new List<Sites>();
+            using (var webClient = new WebClient())
+            {
+                try
+                {
+                    webClient.Encoding = Encoding.UTF8;
+                    response = webClient.DownloadString(Resources.GetSites);
+                    sites = JsonConvert.DeserializeObject<List<Sites>>(response);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+                return sites.OrderBy(n => n.Name).ToList();
+            }
+        }
+    }
+}
