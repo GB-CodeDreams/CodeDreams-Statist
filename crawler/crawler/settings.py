a1713afd1d937f6924ee8049b79364cf80bcfa26
indablackk <indablackk@gmail.com>
2016-04-11 21:03:11 +0400

    Добавил кодировку

--- crawler/crawler/settings.py
+++ crawler/crawler/settings.py
@@ -7,6 +7,7 @@ DB_settings = {
         'password': '4a3b98d8',
         'host': 'us-cdbr-iron-east-03.cleardb.net',
         'port': ':3306',
-        'database': 'heroku_e87588648c44590'
+        'database': 'heroku_e87588648c44590',
+        'charset': 'charset=utf8mb4'
     }
 # Database engine: '{dialect}+{driver}://{user}:{password}@{host}{port}/{database}'
