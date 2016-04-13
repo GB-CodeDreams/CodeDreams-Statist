2e9aaccd10a66c22844e11fd77b4453b8cbad337
shtylev <shtylevAA@rambler.ru>
2016-04-13 09:54:06 +0300

    Добавил метод получения общей статистики. Добавлено ключевое слово ref

--- desktop-interface/ui/Statist/Statist/DAL/DBWebService.cs
+++ desktop-interface/ui/Statist/Statist/DAL/DBWebService.cs
@@ -12,10 +12,9 @@ namespace Statist.DAL
 {
     public static class DBWebService
     {
-        public static List<Persons> GetPersons()
+        public static List<Persons> GetPersons(ref List<Persons> persons)
         {
             string response = "";
-            List<Persons> persons = new List<Persons>();
             using (var webClient = new WebClient())
             {
                 try
@@ -31,10 +30,9 @@ namespace Statist.DAL
             }
             return persons.OrderBy(n => n.Name).ToList();
         }
-        public static List<Sites> GetSites()
+        public static List<Sites> GetSites(ref List<Sites> sites)
         {
             string response = "";
-            List<Sites> sites = new List<Sites>();
             using (var webClient = new WebClient())
             {
                 try
@@ -49,6 +47,25 @@ namespace Statist.DAL
                 }
 
                 return sites.OrderBy(n => n.Name).ToList();
+            }
+        }
+        public static List<GeneralStatistics> GetGeneralStatistics(string nameSite, ref List<GeneralStatistics> generalStatistics)
+        {
+            string response = "";
+            using (var webClient = new WebClient())
+            {
+                try
+                {
+                    webClient.Encoding = Encoding.UTF8;
+                    response = webClient.DownloadString(Resources.GetGeneralStatistics + nameSite);
+                    generalStatistics = JsonConvert.DeserializeObject<List<GeneralStatistics>>(response);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+                return generalStatistics;
             }
         }
     }
