2472cd5370b8ad026b375851cb3b19427289b446
Kirill <kerzo@mail.ru>
2016-04-14 06:48:27 +0000

    create table users

--- web-service/db/migrate/20160413075625_create_user.rb
+++ web-service/db/migrate/20160413075625_create_user.rb
@@ -1,4 +1,9 @@
 class CreateUser < ActiveRecord::Migration
   def change
+    create_table :users do |t|
+      t.string  :username
+      t.string  :password
+      t.integer :admin, limit: 1, default: 0
+    end
   end
 end
--- web-service/db/schema.rb
+++ web-service/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160406134137) do
+ActiveRecord::Schema.define(version: 20160413075625) do
 
   create_table "keywords", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -56,6 +56,12 @@ ActiveRecord::Schema.define(version: 20160406134137) do
     t.string   "name",       limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string  "username", limit: 255
+    t.string  "password", limit: 255
+    t.integer "admin",    limit: 1,   default: 0
   end
 
   add_foreign_key "keywords", "persons"
