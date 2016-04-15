710c98ae7366ddd5a8b82dc3321f08fc1151ebf8
Kirill <kerzo@mail.ru>
2016-04-15 09:07:38 +0000

    для создания или редактирования элемента таблицы sites необходимо передавать параметр url

--- web-service/controller/sites_controller.rb
+++ web-service/controller/sites_controller.rb
@@ -1,22 +1,39 @@
 
 post "/sites" do
-  body = request.POST
-  site = Site.new(name: body["name"])
+  return [400, [error: {pages: ["url can't be blank"]}].to_json] if form_data["url"].nil? || form_data["url"].empty?
+  site = Site.new(name: form_data["name"])
   if site.save
+    site.pages.create(url: form_data["url"])
     Site.all.to_json
   else
-    [{error: site.errors.messages}].to_json
+    [400, [error: site.errors.messages].to_json]
   end
 end
-
+ 
+#тяжелый метод, в одном запросе могут передаваться данные для двух таблиц
 patch "/sites/:id" do
-  body = request.POST
   site = Site.find_by(id: params[:id])
-  return [{error: {sites: ["site not found"]}}].to_json unless site
-  if site.update_attributes(body)
-    Site.all.to_json
-  else
-    [{error: site.errors.messages}].to_json
+  return [400, [error: {sites: ["site not found"]}].to_json] unless site
+  if form_data["name"] && form_data["url"]
+    page = site.pages.first
+    if site.update_attributes(name: form_data["name"]) && page.update_attributes(url: form_data["url"])
+      Site.all.to_json
+    else
+      [400, [error: [site, page].find{|i| !i.valid?}.errors.messages].to_json ]
+    end
+  elsif form_data["name"]
+    if site.update_attributes(name: form_data["name"])
+      Site.all.to_json
+    else
+      [400, [error: site.errors.messages].to_json]
+    end
+  else form_data["url"]
+    page = site.pages.first
+    if page.update_attributes(url: form_data["url"])
+      Site.all.to_json
+    else
+      [400, [error: page.errors.messages].to_json]
+    end
   end
 end
 
