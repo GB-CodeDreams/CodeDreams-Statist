617ec015fe57beb5ce234f3db2a86522f96a69b7
indablackk <indablackk@gmail.com>
2016-04-16 13:20:14 +0400

    Переименовал класс краулера. Добавил общий обработчик ошибок.

--- crawler/crawler/main.py
+++ crawler/crawler/main.py
@@ -14,7 +14,7 @@ from sqlalchemy.orm import sessionmaker
 from reppy.cache import RobotsCache
 
 
-def create_mysql_session(db_settings_dict):
+def create_db_session(db_settings_dict):
     DB = db_settings_dict
     engine_db = '{dialect}+{driver}://{user}:{password}@{host}/{database}?{charset}'\
                 .format(dialect=DB['dialect'], driver=DB['driver'],
@@ -26,8 +26,8 @@ def create_mysql_session(db_settings_dict):
     return Session()
 
 
-class CrawlEvery(threading.Thread):
-    def __init__(self, interval, delay):
+class Crawler(threading.Thread):
+    def __init__(self, interval=86400, delay=0):
         threading.Thread.__init__(self)
         self.daemon = True
         self.interval = interval
@@ -40,12 +40,12 @@ class CrawlEvery(threading.Thread):
             print("Start crawling at %s" % datetime.utcnow())
 
             robots_cache = RobotsCache()  # не обязательно, кэш для быстродействия
-            session = create_mysql_session(DB)  # создаем сессию БД с настройками DB
+            session = create_db_session(DB)  # создаем сессию БД с настройками DB
 
             # из sitemap в БД пишем все новые страницы
-            parse_sitemap_to_db(session=session, robots=robots_cache)
+            parse_sitemap_to_db(session=session)
             # парсим новые страницы из БД на рейтинги
-            parse_pages_from_bd(session=session)
+            parse_pages_from_db(session=session)
             # засыпаем на self.interval часов
             time.sleep(self.interval)
 
@@ -54,16 +54,25 @@ class CrawlEvery(threading.Thread):
 
 
 def main():
-    # ппосчитаем задержку первого запуска в секундах до 23:30:00
-    utc_now = datetime.utcnow()
-    delay_sec = (utc_now.replace(hour=23, minute=30, second=0, microsecond=0) - utc_now).total_seconds()
+    try:
+        # ппосчитаем задержку первого запуска в секундах до 23:30:00
+        utc_now = datetime.utcnow()
+        delay_sec = (utc_now.replace(hour=23, minute=30, second=0, microsecond=0) - utc_now).total_seconds()
 
-    # интервал заупска краулера - сутки(в секундах)
-    interval_sec = 86400
+        # интервал заупска краулера - сутки(в секундах)
+        interval_sec = 86400
 
-    crawler = CrawlEvery(interval_sec, delay_sec)
-    crawler.start()
-
+        crawler = Crawler(interval_sec, delay_sec)
+        # crawler = Crawler(60, 5)
+        crawler.start()
+    except Exception as e:
+        print("Возникла ошибка!")
+        print(e.message, e.args)
+        print("Повторим проход цикла краулера.")
+        main()
+    except Exception as e:
+        print(e.message, e.args)
+        print("Не удалось решить проблему.")
 
 if __name__ == '__main__':
     main()
