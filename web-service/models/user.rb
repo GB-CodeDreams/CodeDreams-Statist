4c29fa9a5bc45419788f6f10134b2f65e70c4a08
vladvad <francuz006@gmail.com>
2016-04-21 20:27:58 +0300

    Revert "added email column and validation to user"
    This reverts commit d1e1082f9b17d231e42bf2c333c179ee8e3cc6b5.

--- web-service/models/user.rb
+++ web-service/models/user.rb
@@ -4,18 +4,12 @@ class User < ActiveRecord::Base
 
   SALT = "geekbrains"
 
-  email_regex = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-
   has_many :persons, dependent: :destroy
   has_many :sites,   dependent: :destroy
 
   validates :username, presence:   true
   validates :username, uniqueness: true
   validates :password, presence:   true, length: {minimum: 6}
-  validates :email,    presence:   true,
-                       uniqueness: true,
-                       format: { with: email_regex }
-
 
   before_save :password_to_md5, if: Proc.new {|user| user.password_changed? }
 
