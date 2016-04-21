d1e1082f9b17d231e42bf2c333c179ee8e3cc6b5
vladvad <francuz006@gmail.com>
2016-04-21 18:52:34 +0300

    added email column and validation to user

--- web-service/db/schema.rb
+++ web-service/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160415064431) do
+ActiveRecord::Schema.define(version: 20160421175015) do
 
   create_table "keywords", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 20160415064431) do
     t.string  "username", limit: 255
     t.string  "password", limit: 255
     t.boolean "admin",                default: false
+    t.string  "email",    limit: 255,                 null: false
   end
 
   add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
