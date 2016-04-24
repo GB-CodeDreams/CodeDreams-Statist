35a1f979d19bae255fb0f972993966ddfab87c9a
shtylev <shtylevAA@rambler.ru>
2016-04-24 20:38:23 +0300

    Добавление класса User

---
+++ desktop-interface/ui/Statist/Statist/Model/User.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Statist.Model
+{
+    public static class User
+    {
+        [JsonProperty(PropertyName = "id")]
+        public static string Id { get; set; }
+
+        [JsonProperty(PropertyName = "username")]
+        public static string Name { get; set; }
+
+        [JsonProperty(PropertyName = "token")]
+        public static string Token { get; set; }
+    }
+}
